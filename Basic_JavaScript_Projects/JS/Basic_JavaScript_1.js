cfa989110a984e7aa6058784cd81e3c0078536a3
VinceCeni <64444955+VinceCeni@users.noreply.github.com>
2020-06-03 18:17:33 -0400

    Add files via upload

--- Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -1,2 +1,8 @@
-var x = "This is a string";
-document.write(x);
+var Family = "The Arrezinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);//Defining some variables
+window.alert("I'm a software developer!")//Telling a window to display the text
+var blues = "I have the blues.";//Assigning a variable
+var blues = blues.fontcolor("blue");//Assigning font color to a variable
+document.write(blues);//Telling it to display this variable
+document.write(3 + 3);//Telling it to display the value associated with this expression
+document.write(Family + Mom);
