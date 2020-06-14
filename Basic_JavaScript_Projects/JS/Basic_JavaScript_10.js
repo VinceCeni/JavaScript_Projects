abae5de72d16d4dddfaef7890680365e60c35147
VinceCeni <64444955+VinceCeni@users.noreply.github.com>
2020-06-14 16:24:34 -0400

    Add files via upload

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_10.js
@@ -0,0 +1,54 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function array_function() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Array").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+var Z = 82;
+document.write(Z);
+{
+    let Z = 33;
+    document.write("<br>" + Z);
+}
+document.write("<br>" + Z);
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
